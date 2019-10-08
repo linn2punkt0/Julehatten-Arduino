af9d52c6f58f8e88b631aaf4f9c5c9954fcbf424
Linn <linn.johansson7@hotmail.com>
2019-10-08 11:59:19 +0200

    add soundTest and working demo

--- Julehatten.ino
+++ Julehatten.ino
@@ -1,50 +1,114 @@
 // define all Pins
 // int colorPin = ;
-int temperaturePin= 6;
+int temperaturePin = 10;
 // int firePin= ;
-// int soundPin= ;
-// int lightPin= ;
-int temperatureVal= 4;
+int soundPin = 9;
+// int lightPin = 12;
+int temperatureVal = 4;
+int temperatureC = 25;
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define PIN 12
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-// put your setup code here, to run once:
+  // put your setup code here, to run once:
 
-// OUTPUTS
-// pinMode(soundPin, OUTPUT);
-// pinMode(lightPin, OUTPUT);
+  // OUTPUTS
+  pinMode(soundPin, OUTPUT);
+  // pinMode(lightPin, OUTPUT);
 
-// INPUTS
-// pinMode(colorPin, INPUT);
-pinMode(temperaturePin, INPUT);
-// pinMode(firePin, INPUT);
+  byte neoBright = 100;
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.show();   // Send the updated pixel colors to the hardware.
+  pixels.setBrightness(neoBright);
+
+  // INPUTS
+  // pinMode(colorPin, INPUT);
+  pinMode(temperaturePin, INPUT);
+  // pinMode(firePin, INPUT);
+
+  Serial.begin(9600);
+}
+
+float take_averaged_reading(int temp)
+{
+  float sum = 0.0 ;
+  int counts = 8;
+  for (byte i = 0 ; i < counts ; i++)
+    sum += temp ; // whatever code reads the sensor
+  return sum / counts ;
+}
+
+float measure_temperature()
+{
+  //getting the voltage reading from the temperature sensor
+  int reading = analogRead(temperaturePin);
+
+  // converting that reading to voltage, for 3.3v arduino use 3.3
+  float voltage = reading * 3.3;
+  voltage /= 1024.0;
+
+  //converting from 10 mv per degree wit 500 mV offset
+  //to degrees ((voltage - 500mV) times 100)
+  float temperatureC = (voltage - 0.5) * 100 ;
+
+  int average;
+  average = take_averaged_reading(temperatureC);
+  Serial.println(average);
+  return average;
+  delay(1000);
+
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
+  int temperature;
+  temperature = measure_temperature();
 
-// Read all input values
-// colorVal = digitalRead(colorPin);
-temperatureVal = analogRead(temperaturePin);
-delay(400);
-// fireVal = digitalRead(firePin);
+  // check if temperature is below the specified degrees
+  if (temperature <= 20) {
+    Serial.println("Its christmas! ");
 
-// check if temperature is below 0 degrees
-if(temperatureVal <= 0,5){
-  Serial.print(temperatureVal);
-// turn on and off lights
-// digitalWrite(lightPin, HIGH);
-// delay(400);
-// digitalWrite(lightPin, LOW);
-// delay(400);
+    // turn on the jingle bells!
+    digitalWrite(soundPin, HIGH);
 
-// turn on the jingle bells!
-// digitalWrite(soundPin, HIGH);
-}
-else{
-Serial.print("too warm");
-// turn off sound and lights
-// digitalWrite(lightPin, LOW);
-// digitalWrite(soundPin, LOW);
-}
+    //turn on and off lights
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+      // Set green color:
+      pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+      pixels.show();
+      delay(300);
+    }
+    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+      // Setting white color:
+      pixels.setPixelColor(i, pixels.Color(255, 255, 153));
+      pixels.show();
+      delay(300);
+    }
+    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+      // Setting red color:
+      pixels.setPixelColor(i, pixels.Color(204, 0, 0));
+      pixels.show();
+      delay(300);
+    }
+  }
+  else {
+    Serial.println("too warm");
+    // turn off sound and lights
+    pixels.clear();
+      for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+      // Setting red color:
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+      pixels.show();
+    }
+    digitalWrite(soundPin, LOW);
+    delay(900);
+  }
 }
---
+++ soundTest/soundTest.ino
@@ -0,0 +1,18 @@
+int soundPin= 9;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(soundPin, OUTPUT);
+} 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// digitalWrite(soundPin, HIGH);
+delay(600);
+digitalWrite(soundPin, LOW);
+delay(600);
+//  digitalWrite(soundPin, HIGH);
+delay(600);
+digitalWrite(soundPin, LOW);
+  
+}
